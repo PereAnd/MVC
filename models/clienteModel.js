02e88040e290f9f23aa8fb8f17231a3e52b3d14f
MSLegholas <jonhatanromero@cbit-online.com>
2024-02-26 10:53:48 -0500

    cambio minus en atributo billetera/ClienteModel

--- models/clienteModel.js
+++ models/clienteModel.js
@@ -43,7 +43,7 @@ const Cliente = sequilize.define(
             type: DataTypes.STRING,
             allowNull: false
         },
-        Billetera:{
+        billetera:{
             type: DataTypes.ENUM,
             allowNull: false
         }
