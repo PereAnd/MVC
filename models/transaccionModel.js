e1cda78145760fa2febbdd2cd66c2758a50451f3
MSLegholas <jonhatanromero@cbit-online.com>
2024-02-28 10:50:34 -0500

    adicion dos clases tipoEntidadFModel/transaccionModel

---
+++ models/transaccionModel.js
@@ -0,0 +1,53 @@
+const {sequelize} = require("../config/mysql");
+const {DataTypes} = require("sequelize");
+
+const Transaccion = sequelize.define(
+    "transaccion",
+    {
+        idTransaccion:{
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+            type: DataTypes.INTEGER
+        },
+        idCuenta:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        idEntidadFinanciera:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        fechaTransaccion:{
+            type: DataTypes.DATE
+        },
+        montoTransaccion:{
+            type: DataTypes.DOUBLE
+        },
+        destinoPago:{
+            type: DataTypes.STRING
+        },
+        motivo:{
+            type: DataTypes.STRING    
+        },
+        estadoTransaccion:{
+            type: DataTypes.STRING
+        },
+        idTransaccionAutorizador:{
+            type:DataTypes.STRING
+        },
+        numeroAprobacion:{
+            type:DataTypes.STRING
+        },
+        idError:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+module.exports.Transaccion;
