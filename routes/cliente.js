5636819e639fabb7c389d24da2daebdfb4ce68fc
alvaroperez <alvaroperez@cbit-online.com>
2024-02-28 17:34:42 -0500

    CRUD Clientes Ok

--- routes/cliente.js
+++ routes/cliente.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getCliente,
+  getClientes,
+  createCliente,
+  updateCliente,
+  deleteCliente
+} = require("../controllers/clienteController");
 
-const {getCliente, getClientes, createCliente, updateCliente,deleteCliente} = require("../controllers/clienteController");
-// se complementa con la url de app.js
-router.get("/:id", getCliente);
-router.get("/"),getClientes;
-router.post("/",createCliente);
-router.put("/:id",updateCliente);
-router.delete("/:id",deleteCliente);
-
+router.get('/', getClientes);
+router.get('/:id', getCliente);
+router.post('/', createCliente);
+router.put('/:id', updateCliente);
+router.delete('/:id', deleteCliente);
 
 module.exports = router;
