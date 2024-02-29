b7e1296084498bc6af72a62458257bfc0dbf42fc
alvaroperez <alvaroperez@cbit-online.com>
2024-02-29 14:52:50 -0500

    Solucion de errores

--- routes/cuenta.js
+++ routes/cuenta.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 
 const {
-    getCuenta,
-    getCuentas,
-    createCuenta,
-    updateCuenta,
-    deleteCuenta
+  getCuenta,
+  getCuentas,
+  createCuenta,
+  updateCuenta,
+  deleteCuenta,
 } = require("../controllers/CuentaController");
 
-router.get('/',getCuenta);
-router.get('/:id',getCuentas);
-router.post('/',createCuenta);
-router.put('/:id',updateCuenta);
-router.delete('/:id',deleteCuenta);
+router.get("/", getCuenta);
+router.get("/:id", getCuentas);
+router.post("/", createCuenta);
+router.put("/:id", updateCuenta);
+router.delete("/:id", deleteCuenta);
 
 module.exports = router;
