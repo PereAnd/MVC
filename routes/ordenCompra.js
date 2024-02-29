ce9f814a01216fd42d8159f40cc64f63a39d4a65
MSLegholas <jonhatanromero@cbit-online.com>
2024-02-29 12:42:41 -0500

    creacion de archivos routes(cuenta,estado,orden compra y transaccion)

---
+++ routes/ordenCompra.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router;
+
+const {
+    getOrdenCompra,
+    getOrdenesCompras,
+    createOrdenCompra,
+    updateOrdenCompra,
+    deleteOrdenCompra
+} = require("../controllers/OrdenCompraController");
+
+router.get('/',getOrdenCompra);
+router.get('/:id',getOrdenesCompras);
+router.post('/',createOrdenCompra);
+router.put('/:id',updateOrdenCompra);
+router.delete('/:id',deleteOrdenCompra);
+
+module.exports = router;
