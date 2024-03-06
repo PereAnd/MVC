86ec93b29ac7a68e496d68270c8c712b5fd75a30
PereAnd <aa.pereza1@uniandes.edu.co>
2024-03-06 16:34:50 -0500

    Validaciones billetera, cuenta, ecommerce

--- controllers/cuentaController.js
+++ controllers/cuentaController.js
@@ -1,137 +1,123 @@
-const {cuentaModel} = require("../models/indexModel");
-/**
- * Obtener cuenta financiera por medio de un Id
- * @param {*} req 
- * @param {*} res 
- */
-const getCuenta = async(req,res)=>{
-    try{
-        const idCuentaC = req.params.id;
-        const cuenta = await cuentaModel.findOne({
-            where:{
-                idCuenta:idCuentaC
-            }
-        });
-        if(!cuenta){
-            res.status(404).send({
-                message:"Cuenta con el id "+idCuentaC+" no existe!!!"
-            });
-        }else{
-            res.status(200).send(cuenta);
-        }
-    }catch(e){
-        res.status(404).send(e);
-    }
-};
-/**
- * obtener lista de cuentas financiera
- * @param {*} req 
- * @param {*} res 
- */
-const getCuentas = async(req,res)=>{
-    try{
-        const cuentas = await cuentaModel.findAll();
-        if(cuentas == null){
-            res.status(404).send({
-                message: "No se han encontrado Cuentas"
-            });
-        }else{
-            res.status(200).send(cuentas);
-        }
-    }
-    catch(e){
-        res.status(404).send(e);
-    }
-};
-/**
- * crear una cuenta financiera
- * @param {*} req 
- * @param {*} res 
- */
-const createCuenta = async(req,res)=>{
-    try{
-        const { body } = req;
-        if(Object.keys(body).length == 0){
-            res.status(404).send({
-                message:"parametros de creación cuenta vacios!!!"
-            });
-        }else{
-            const cuenta = await cuentaModel.create(body);
-            res.status(200).send(cuenta);
-        }
-    }catch(e){
-        res.status(404).send({
-            message:"No se pudo crear la cuenta!!!"
-        });
-    }
-};
-/**
- * Modificar datos de una cuenta financiera
- * @param {*} req 
- * @param {*} res 
- */
-const updateCuenta = async(req,res)=>{
-    try{
-        //veririfcar si pidiendo todo el body y deconstruyendolo con idCuenta, sirve?
-        const {body} = req;
-        const idCuenta = req.params.id;
-        const cuenta = await cuentaModel.findOne({
-            where: {
-                idCuenta: idCuenta
-            }
-        });
-        if(Object.keys(body).length == 0){
-            res.status(404).send({
-                message: "parametros de creación cuenta vacios!!!"
-            });
-        }else if(!cuenta){
-            res.status(404).send({
-                message: "No se encontró cuenta financiera con el id "+idCuenta
-            });
-        }else{
-            const{body} = req;
-            cuenta.usuario = body.usuario;
-            cuenta.password = body.password;
-            cuenta.numeroCuenta = body.numeroCuenta;
-            cuenta.idEntidadFinanciera = body.idEntidadFinanciera;
-            cuenta.idEstado = body.idEstado;
-            await cuenta.save();
-            res.status(200).send({
-                message:"Cuenta con id "+idCuenta+", ha sido modificada!!!"
-            });
-        }
-    }catch(e){
-        res.status(404).send({
-            error: e,
-            message: "No se encontró cuenta financiera con el id "+idCuenta
-        });
-    }
-};
-/**
- * Borrar cuenta financiera con id
- */
+const { cuentaModel } = require("../models/indexModel");
+const { Op } = require("sequelize");
 
-const deleteCuenta = async (req,res)=>{
-    try{
-        const idCuentaF = req.params.id;
-        const cuenta = await cuentaModel.findOne({
-            where:{
-                idCuenta: idCuentaF
-            }
-        });
-        if(!cuenta){
-            res.status(404).send({
-                message: "No se encontró cuenta financiera con el id "+idCuentaF
-            });
-        }else{
-            await cuenta.destroy();
-            res.status(200).send({
-                message: "Se ha eliminado la cuenta con id "+idCuentaF+", correctamente!!!"
-            });
-        }
+const getCuentas = async (req, res) => {
+  try {
+    const usuario = req.query.usuario;
+    if (!usuario) {
+      const data = await cuentaModel.findAll();
+      res.send(data);
+    } else {
+      const data = await cuentaModel.findOne({
+        where: {
+          usuario,
+        },
+      });
+      if (!data) return res.status(404).send({ error: "Cuenta no encontrada" });
+      else res.send(data);
     }
-    catch(e){
-        res.status(404).send(e);
-    }
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
-module.exports = {getCuenta,getCuentas,createCuenta,updateCuenta,deleteCuenta};
+
+const getCuenta = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await cuentaModel.findOne({
+      where: {
+        idCuenta: id,
+      },
+    });
+    if (!data) return res.status(404).send({ error: "Cuenta no encontrada" });
+    res.send(data);
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
+};
+
+const createCuenta = async (req, res) => {
+  const {usuario, password, numeroCuenta, idEstado, idEntidadFinanciera, idCliente} = req.body;
+  try {
+    if (!usuario || !password || !numeroCuenta || !idEstado || !idEntidadFinanciera || !idCliente)
+      return res.status(400).send({ error: "Datos incompletos" });
+    else {
+      const [cuenta, created] = await cuentaModel.findOrCreate({
+        where: {
+          idEntidadFinanciera: idEntidadFinanciera,
+          numeroCuenta: numeroCuenta,
+        },
+        defaults: {
+          usuario: usuario,
+          password: password,
+          numeroCuenta: numeroCuenta,
+          idEntidadFinanciera: idEntidadFinanciera,
+          idEstado: idEstado,
+          idCliente: idCliente,
+        },
+      });
+      if (!created) return res.status(409).send({ error: "Cuenta ya existe" });
+      else res.status(201).send(cuenta);
+    }
+  } catch (error) {
+    return res.status(500).send({ error: error });
+  }
+};
+
+const updateCuenta = async (req, res) => {
+  const id = req.params.id;
+  const { body } = req;
+  let cuenta = await cuentaModel.findOne({
+    where: {
+      idCuenta: id,
+    },
+  });
+  try {
+    if (cuenta) {
+      if (Object.keys(body).length === 0)
+        return res.status(400).send({ error: "No hay datos para actualizar" });
+      else if (body.numeroCuenta || body.idEntidadFinanciera) {
+        const cuentaExists = await cuentaModel.findOne({
+          where: {
+            [Op.or]: [
+              {
+                idEntidadFinanciera: body.idEntidadFinanciera || cuenta.idEntidadFinanciera,
+                numeroCuenta: body.numeroCuenta || cuenta.numeroCuenta,
+              },
+            ],
+          },
+        });
+        if (cuentaExists && cuentaExists.idCuenta != id)
+          return res.status(409).send({ error: "Cuenta ya existe" });
+      }
+
+      await cuenta.update(body);
+      cuenta.save();
+      res.send(cuenta);
+    } else {
+      return res.status(404).send({ error: "Cuenta no encontrada" });
+    }
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
+};
+
+const deleteCuenta = async (req, res) => {
+  const id = req.params.id;
+  const cuenta = await cuentaModel.findOne({
+    where: {
+      idCuenta: id,
+    },
+  });
+  if (!cuenta)
+    return res.status(404).send({ error: "Cuenta no encontrada" });
+  await cuenta.destroy();
+  res.status(200).send({ message: "Cuenta eliminada correctamente" });
+};
+module.exports = {
+  getCuenta,
+  getCuentas,
+  createCuenta,
+  updateCuenta,
+  deleteCuenta,
+};
--- controllers/ecommerceController.js
+++ controllers/ecommerceController.js
@@ -1,42 +1,86 @@
 const { ecommerceModel } = require("../models/indexModel");
+const { Op } = require("sequelize");
 
 const getEcommerces = async (req, res) => {
-  const data = await ecommerceModel.findAll();
-  res.send(data);
+  try {
+    const data = await ecommerceModel.findAll();
+    res.send(data);
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
 
 const getEcommerce = async (req, res) => {
-  const id = req.params.id;
-  const data = await ecommerceModel.findOne({
-    where: {
-      idEcommerce: id,
-    },
-  });
-  if (!data) return res.status(404).send({ error: "Ecommerce no encontrado" });
-  res.send(data);
+  try {
+    const id = req.params.id;
+    const data = await ecommerceModel.findOne({
+      where: {
+        idEcommerce: id,
+      },
+    });
+    if (!data)
+      return res.status(404).send({ error: "Ecommerce no encontrado" });
+    res.send(data);
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
 
 const createEcommerce = async (req, res) => {
   const { body } = req;
-  console.log(body);
-  const data = await ecommerceModel.create(body);
-  res.send( data );
+  try {
+    if (!body.nombre)
+      return res.status(400).send({ error: "Datos incompletos" });
+    else {
+      const [ecommerce, created] = await ecommerceModel.findOrCreate({
+        where: {
+          [Op.or]: [body],
+        },
+        defaults: {
+          nombre: body.nombre,
+        },
+      });
+      if (!created)
+        return res.status(409).send({ error: "Ecommerce ya existe" });
+      else res.status(201).send(ecommerce);
+    }
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
 
 const updateEcommerce = async (req, res) => {
   const id = req.params.id;
-  const ecommerce = await ecommerceModel.findOne({
+  const { body } = req;
+  let ecommerce = await ecommerceModel.findOne({
     where: {
       idEcommerce: id,
     },
   });
-  if (!ecommerce)
-    return res.status(404).send({ error: "Ecommerce no encontrado" });
-  let { body } = req;
-  ecommerce.nombre = body.nombre;
-
-  await ecommerce.save();
-  res.send(ecommerce);
+  try {
+    if (ecommerce) {
+      if (Object.keys(body).length === 0) {
+        return res.status(400).send({ error: "No hay datos para actualizar" });
+      } else if (!body.nombre) {
+        return res.status(400).send({ error: "Datos incompletos" });
+      } else {
+        const ecommerceExists = await ecommerceModel.findOne({
+          where: {
+            [Op.or]: [body],
+          },
+        });
+        if (ecommerceExists && ecommerceExists.idEcommerce != id)
+          return res.status(409).send({ error: "Ecommerce ya existe" });
+      }
+      await ecommerce.update(body);
+      ecommerce.save();
+      res.send(ecommerce);
+    } else {
+      return res.status(404).send({ error: "Ecommerce no encontrado" });
+    }
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
 
 const deleteEcommerce = async (req, res) => {
@@ -48,8 +92,8 @@ const deleteEcommerce = async (req, res) => {
   });
   if (!ecommerce)
     return res.status(404).send({ error: "Ecommerce no encontrado" });
-	await ecommerce.destroy();
-  res.send({message: 'Ecommerce eliminado correctamente'});
+  await ecommerce.destroy();
+  res.status(200).send({ message: "Ecommerce eliminado correctamente" });
 };
 
 module.exports = {
