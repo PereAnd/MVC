8ad9c736f176425e51281c3b01ba26651f7b4758
MSLegholas <jonhatanromero@cbit-online.com>
2024-03-18 15:18:26 -0500

    prueba moficiando json transaccionModel

--- models/transaccionModel.js
+++ models/transaccionModel.js
@@ -2,35 +2,36 @@ const {sequelize} = require("../config/mysql");
 const {DataTypes} = require("sequelize");
 
 const Transaccion = sequelize.define(
-    "transaccion",
-    {
-        idTransaccion:{
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-            type: DataTypes.INTEGER
-        },
-        idCuenta:{
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        montoTransaccion:{
-            type: DataTypes.DOUBLE
-        },
-        destinoPago:{
-            type: DataTypes.STRING
-        },
-        motivo:{
-            type: DataTypes.STRING    
-        },
-        idEstado:{
-            type: DataTypes.INTEGER
-        },
-        idTransaccionAutorizador:{
-            type:DataTypes.STRING
-        },
-        numeroAprobacion:{
-            type:DataTypes.STRING
+    "transaccion",{
+    transaccion: {
+            idTransaccion:{
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+                type: DataTypes.INTEGER
+            },
+            idCuenta:{
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            montoTransaccion:{
+                type: DataTypes.DOUBLE
+            },
+            destinoPago:{
+                type: DataTypes.STRING
+            },
+            motivo:{
+                type: DataTypes.STRING    
+            },
+            idEstado:{
+                type: DataTypes.INTEGER
+            },
+            idTransaccionAutorizador:{
+                type:DataTypes.STRING
+            },
+            numeroAprobacion:{
+                type:DataTypes.STRING
+            }
         }
     },
     {
