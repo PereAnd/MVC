b6122eb0bfe453c0b81319ff263a1b288cf79f4c
MSLegholas <jonhatanromero@cbit-online.com>
2024-03-18 15:25:11 -0500

    reversar cambio transaccionModel

--- models/transaccionModel.js
+++ models/transaccionModel.js
@@ -2,36 +2,35 @@ const {sequelize} = require("../config/mysql");
 const {DataTypes} = require("sequelize");
 
 const Transaccion = sequelize.define(
-    "transaccion",{
-    transaccion: {
-            idTransaccion:{
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false,
-                type: DataTypes.INTEGER
-            },
-            idCuenta:{
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            montoTransaccion:{
-                type: DataTypes.DOUBLE
-            },
-            destinoPago:{
-                type: DataTypes.STRING
-            },
-            motivo:{
-                type: DataTypes.STRING    
-            },
-            idEstado:{
-                type: DataTypes.INTEGER
-            },
-            idTransaccionAutorizador:{
-                type:DataTypes.STRING
-            },
-            numeroAprobacion:{
-                type:DataTypes.STRING
-            }
+    "transaccion",
+    {
+        idTransaccion:{
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+            type: DataTypes.INTEGER
+        },
+        idCuenta:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        montoTransaccion:{
+            type: DataTypes.DOUBLE
+        },
+        destinoPago:{
+            type: DataTypes.STRING
+        },
+        motivo:{
+            type: DataTypes.STRING    
+        },
+        idEstado:{
+            type: DataTypes.INTEGER
+        },
+        idTransaccionAutorizador:{
+            type:DataTypes.STRING
+        },
+        numeroAprobacion:{
+            type:DataTypes.STRING
         }
     },
     {
