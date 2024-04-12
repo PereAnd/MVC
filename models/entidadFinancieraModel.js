8291ecc64f448e159a7f540e0b40d7145ee4c3be
PereAnd <aa.pereza1@uniandes.edu.co>
2024-04-12 17:02:53 -0500

    Modificación modelo Entidad financiera

--- models/entidadFinancieraModel.js
+++ models/entidadFinancieraModel.js
@@ -14,6 +14,10 @@ const EntidadFinanciera = sequelize.define(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    imagen: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     nit: {
       type: DataTypes.STRING,
       allowNull: false,
