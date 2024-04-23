08013c0688b51cfcbc090a276c1674f23f3d8e9f
MSLegholas <jonhatanromero@cbit-online.com>
2024-04-23 11:08:18 -0500

    ajuste codigo app.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const  bodyParser = require('body-parser');
 const app = express();
 app.use(cors())
 //linea de codigo que captura por medio de una expre json en body, datos
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(express.json())
 //linea que establece el puesto desde variables de entorno o coloca 3000
 const port = process.env.PORT || 3000;
@@ -16,9 +18,6 @@ const port = process.env.PORT || 3000;
 //invocacion de rutas
 
 app.use("/api", require("./routes"));
-
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 app.listen(port, () =>
   console.log(`Tu server esta listo por el puerto ${port}`)
