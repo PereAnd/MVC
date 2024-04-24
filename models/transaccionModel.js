1824a9c7fc1ae226eb4874a9e90183fdb90bcddf
PereAnd <aa.pereza1@uniandes.edu.co>
2024-04-24 08:17:48 -0500

    Se soluciona el error en el modelo de Transacciones

--- models/transaccionModel.js
+++ models/transaccionModel.js
@@ -1,53 +1,48 @@
-const {sequelize} = require("../config/mysql");
-const {DataTypes} = require("sequelize");
+const { sequelize } = require("../config/mysql");
+const { DataTypes } = require("sequelize");
 
 const Transaccion = sequelize.define(
-    "Transaccion",
-    {
-        idTransaccion:{
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-            type: DataTypes.INTEGER
-        },
-        idTipo_Transaccion:{
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        idEstado:{
-            type: DataTypes.INTEGER
-        },
-        idProducto:{
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        IP:{
-            type: DataTypes.STRING
-        },
-        montoTransaccion:{
-            type: DataTypes.DOUBLE
-        },
-        destinoPago:{
-            type: DataTypes.STRING
-        },
-        motivo:{
-            type: DataTypes.STRING    
-        },
-        idTransaccionAutorizador:{
-            type:DataTypes.STRING
-        },
-        numeroAprobacion:{
-            type:DataTypes.STRING
-        },
-        idOrdenCompra:{
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
+  "Transaccion",
+  {
+    idTransaccion: {
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      type: DataTypes.INTEGER,
     },
-    {
-        timestamps: true
-    }
+    idTipo_Transaccion: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    idEstado: {
+      type: DataTypes.INTEGER,
+    },
+    idProducto: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    IP: {
+      type: DataTypes.STRING,
+    },
+    montoTransaccion: {
+      type: DataTypes.DOUBLE,
+    },
+    destinoPago: {
+      type: DataTypes.STRING,
+    },
+    motivo: {
+      type: DataTypes.STRING,
+    },
+    idTransaccionAutorizador: {
+      type: DataTypes.STRING,
+    },
+    numeroAprobacion: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
-
 
 module.exports = Transaccion;
